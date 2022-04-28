5adb25c97b6269f76276fa42ced60fc72698e1d6
Egor890 <arion890a@yandex.ru>
2022-04-28 11:43:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-jtdmea
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-jtdmea)
---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+<h2>User's list</h2>
+  <div id="app">
+    
+
+    <div class="card" v-for="user in users" :key="user.id.value">
+      <div class="header">
+      <img class="photo card-element" :src="user.picture.thumbnail" alt="photo" />
+       <div class="header_name">{{ user.name.first }} {{ user.name.last }}</div> 
+      </div>
+
+      <div>
+        {{ user.name.title }}.
+        {{ user.name.first }}
+        
+      </div>
+    <div class="Address card-element">
+    Address: {{user.location.country}},{{user.location.city}},{{user.location.street.name}}
+    {{user.location.street.number}}
+    </div>
+    Phone number: {{user.phone}}<br>
+      email: <a href="#">{{ user.email }}</a>
+      <div class="card-element">Age: {{user.registered.age}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+
+  data() {
+    return {
+      users: [],
+    };
+  },
+
+  mounted() {
+    fetch('https://randomuser.me/api/?results=10')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        this.users = data.results;
+      });
+  },
+};
+</script>
+
+<style>
+#app {
+  line-height:1.5;
+  width: 1200px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content:center;
+  flex-wrap:wrap;
+  align-items:center;
+  align-content:center;
+  
+}
+.card:hover{
+  cursor:pointer;
+  transition: 0.3s;
+  transform: scale(1.05);
+  box-shadow: 4px 3px 7px grey;
+}
+.card {
+  box-shadow: 3px 2px 5px grey;
+  margin-left:15px;
+  width:250px;
+  height:300px;
+  padding: 10px 10px 0px;
+  border-radius: 9px;
+  border:2px solid black;
+  margin-bottom: 15px;
+}
+
+.photo {
+  border: 4px solid black;
+  border-radius: 50px;
+}
+.header{
+  display:flex;
+  padding: 0px 30px 5px 20px;
+  align-items:center;
+  justify-content:space-around;
+  margin-bottom:10px;
+   border-bottom:2px solid black;
+}
+a{
+color:black;
+text-decoration:none;
+}
+</style>
+
